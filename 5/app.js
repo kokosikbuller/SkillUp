dd4ee6c79db557fa5c94a9fd28085d230a79e927
Kostya Rizun <k.rizun@splinestudio.ua>
2021-07-22 14:58:27 +0300

    change name country

--- 5/app.js
+++ 5/app.js
@@ -1,30 +1,32 @@
 let countryName = prompt('Название страны').toLowerCase();
+let upperName = countryName[0].toUpperCase() + countryName.slice(1);
 let price = 0;
+
 
 switch (countryName) {
     case 'китай':
         price = 150;
-        console.log(`Доставка в ${countryName}, будет стоить ${price} кредитов`);
+        console.log(`Доставка в ${upperName}, будет стоить ${price} кредитов`);
         break;
 
     case 'чили':
         price = 250;
-        console.log(`Доставка в ${countryName}, будет стоить ${price} кредитов`);
+        console.log(`Доставка в ${upperName}, будет стоить ${price} кредитов`);
         break;
 
     case 'австралия':
         price = 165;
-        console.log(`Доставка в ${countryName}, будет стоить ${price} кредитов`);
+        console.log(`Доставка в ${upperName}, будет стоить ${price} кредитов`);
         break;
 
     case 'индия':
         price = 90;
-        console.log(`Доставка в ${countryName}, будет стоить ${price} кредитов`);
+        console.log(`Доставка в ${upperName}, будет стоить ${price} кредитов`);
         break;
 
     case 'ямайка':
         price = 130;
-        console.log(`Доставка в ${countryName}, будет стоить ${price} кредитов`);
+        console.log(`Доставка в ${upperName}, будет стоить ${price} кредитов`);
         break;
 
     default:
