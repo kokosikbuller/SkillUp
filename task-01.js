00d874782103ec2d7f5a755adc36ec93f48d24fe
Kostya Rizun <k.rizun@splinestudio.ua>
2021-07-30 18:53:04 +0300

    add project

--- task-01.js
+++ task-01.js
@@ -1,15 +1,22 @@
-const user = {
-    name: 'Mango',
-    age: 20,
-    hobby: 'html',
-    premium: true,
-};
-
-user.mood = 'happy';
-user.hobby = 'skydiving';
-user.premium = false;
-
-const userKeys = Object.keys(user);
-for(item of userKeys) {
-    console.log(item);
-}
+const account = {
+    owner: 'Mango',
+    balance: 24000,
+    discount: 0.1,
+    orders: ['order-1', 'order-2', 'order-3'],
+    changeDiscount(value) {
+      this.discount = value;
+    },
+    showOrders() {
+      return this.orders;
+    },
+    addOrder(cost, order) {
+      this.balance -= cost;
+      this.orders.push(order);
+    },
+  };
+  account.changeDiscount(0.15);
+  console.log(account.discount); // 0.15
+  console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+  account.addOrder(5000, 'order-4');
+  console.log(account.balance); // 19000
+  console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
--- task-02.js
+++ task-02.js
@@ -1,7 +1,21 @@
-const countProps = function(obj) {
-    return Object.values(obj).length;
-};
+const inventory = {
+    items: ['Knife', 'Gas mask'],
+    add(itemName) {
+      console.log(`Adding ${itemName} to inventory`);
+      this.items.push(itemName);
+    },
+    remove(itemName) {
+      console.log(`Removing ${itemName} from inventory`);
+      this.items = this.items.filter(item => item !== itemName);
+    },
+  };
+  const invokeInventoryAction = function(itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    action.bind(inventory, itemName)();
+  };
 
-  console.log(countProps({})); // 0
-  console.log(countProps({ name: 'Mango', age: 2 })); // 2
-  console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+
+  invokeInventoryAction('Medkit', inventory.add);
+  console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+  invokeInventoryAction('Gas mask', inventory.remove);
+  console.log(inventory.items); // ['Knife', 'Medkit']
