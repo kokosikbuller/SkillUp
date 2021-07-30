8786e36cb81bfb5fc931731cd4a4a54df074b385
Kostya Rizun <k.rizun@splinestudio.ua>
2021-07-30 18:23:08 +0300

    remove loop

--- task-03.js
+++ task-03.js
@@ -1,11 +1,17 @@
 const findBestEmployee = function(employees) {
-    const arr = Object.entries(employees);
-    let max = Math.max(...Object.values(employees));
-    for(item of arr) {
-        if(item[1] === max) {
-            return `${item[0]}: ${item[1]}`
-        }
-    }
+    // const arr = Object.entries(employees);
+    // let max = Math.max(...Object.values(employees));
+    // for(item of arr) {
+    //     if(item[1] === max) {
+    //         return `${item[0]}: ${item[1]}`
+    //     }
+    // }
+    const values = Object.values(employees);
+    const keys = Object.keys(employees);
+    const max = Math.max(...values);
+    const index = values.indexOf(max);
+
+    return `${keys[index]}: ${values[index]}`;
 };
 
   console.log(
